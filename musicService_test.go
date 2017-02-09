af0794b2bbb5fda823acfc5ed004a7c23c396f26
Jonas Xavier <jonas.agx@gmail.com>
2017-02-08 17:15:52 -0800

    Adding more tests

--- musicService_test.go
+++ musicService_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"testing"
+	"io/ioutil"
 )
 
 var (
@@ -10,6 +11,18 @@ var (
 	validFilenamesInput = []string{"Pixies  - Where Is My Mind-5iC0YXspJRM.webm", "a.mkv", "kj3lk2j4.mp4"}
 	validFilenamesOutput = []string{"Pixies  - Where Is My Mind-5iC0YXspJRM.mp3", "a.mp3", "kj3lk2j4.mp3"}
 )
+
+func isValidList(validList, testingList []string, t *testing.T) {
+	if len(validList) != len(testingList) {
+		t.Error("Expected", len(validList), " elements but got", (testingList), "instead")
+	}
+
+	for index,value := range testingList {
+		if validList[index] != value{
+			t.Error("Expected", validList[index], "but got", value, "instead")
+		}
+	}
+}
 
 func TestRaplceFunc(t *testing.T){
 	for index,filename := range validFilenamesInput {
@@ -24,13 +37,19 @@ func TestFilterFilenames(t *testing.T) {
 	testableOutput := FilterFilenames(NotFilteredFilenamesInput, `\.txt`)
 	validOutput := []string{"a.txt"}
 
-	if len(validOutput) != len(testableOutput) {
-		t.Error("Expected", len(validOutput), " elements but got", (testableOutput), "instead")
-	}
+	isValidList(validOutput, testableOutput, t)
+}
 
-	for index,value := range testableOutput {
-		if validOutput[index] != value{
-			t.Error("Expected", validOutput[index], "but got", value, "instead")
-		}
-	}
+func TestGetFilesList(t *testing.T) {
+	var path = "."
+	var validFilenames []string
+	var testableOutput = GetFilesList(path)
+
+	infoFiles, _ := ioutil.ReadDir(path)
+    
+    for _,filename := range infoFiles {
+    	validFilenames = append(validFilenames, filename.Name())
+    }
+
+    isValidList(validFilenames, testableOutput, t)
 }
