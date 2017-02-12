a4030deafc4d17ec8978329309c97837eec0ddd1
Jonas Xavier <jonas.agx@gmail.com>
2017-02-11 22:59:32 -0800

    Testing video ID extraction

--- musicService_test.go
+++ musicService_test.go
@@ -10,6 +10,8 @@ var (
 	
 	validFilenamesInput = []string{"Pixies  - Where Is My Mind-5iC0YXspJRM.webm", "a.mkv", "kj3lk2j4.mp4"}
 	validFilenamesOutput = []string{"Pixies  - Where Is My Mind-5iC0YXspJRM.mp3", "a.mp3", "kj3lk2j4.mp3"}
+	youtubeUrls = []string{"https://www.youtube.com/watch?v=HO2tEv68UcY"}
+	videoIDs = []string{"HO2tEv68UcY"}
 )
 
 func isValidList(validList, testingList []string, t *testing.T) {
@@ -20,6 +22,15 @@ func isValidList(validList, testingList []string, t *testing.T) {
 	for index,value := range testingList {
 		if validList[index] != value{
 			t.Error("Expected", validList[index], "but got", value, "instead")
+		}
+	}
+}
+
+func TestGetVideoId(t *testing.T) {
+	for index, value := range youtubeUrls {
+		song := Song{Url: value}
+		if song.GetVideoId() != videoIDs[index] {
+			t.Error("Expected", videoIDs[index], "but got", song.GetVideoId())
 		}
 	}
 }
